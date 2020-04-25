4864c19d490f83c120ce3fa43c6cd55438bacd5e
JonChamplain <47443316+JonChamplain@users.noreply.github.com>
2020-04-24 20:16:45 -0400

    changed mockup link to new heroku url

--- client/src/Work.js
+++ client/src/Work.js
@@ -10,7 +10,7 @@ export default function Work() {
                 </div>
                 <div>
                     <ul className="portfolio-list-grid">
-                        <li className="portfolio-list-item"><a className="portfolio-list-link" href="https://sleepy-anchorage-59081.herokuapp.com/"><img className="portfolio-list-image" src="/images/mockblog_thumbnail.jpg" alt="yelpington page"></img>Mock Blog</a></li>
+                        <li className="portfolio-list-item"><a className="portfolio-list-link" href="https://hidden-fortress-73610.herokuapp.com/"><img className="portfolio-list-image" src="/images/mockblog_thumbnail.jpg" alt="yelpington page"></img>Mock Blog</a></li>
                         <li className="portfolio-list-item"><a className="portfolio-list-link" href="https://secure-fjord-65417.herokuapp.com/"><img className="portfolio-list-image" src="/images/yelpington_thumbnail.jpg" alt="yelpington page"></img>Yelpington</a></li>
                         <li className="portfolio-list-item"><a className="portfolio-list-link" href="https://stark-plateau-02827.herokuapp.com/"><img className="portfolio-list-image" src="/images/triptactoe_thumbnail.jpg" alt="trip-tac-toe game"></img>Trip-Tac-Toe</a></li>
                         <li className="portfolio-list-item"><a className="portfolio-list-link" href="https://github.com/JonChamplain?tab=repositories"><img className="portfolio-list-image" src="/images/github_thumbnail.jpg" alt="github page"></img>GitHub</a></li>
