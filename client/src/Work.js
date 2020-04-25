20ee5efc80fcf051a53120f914cab82825c7f6e4
JonChamplain <47443316+JonChamplain@users.noreply.github.com>
2020-04-24 20:45:48 -0400

    added park burlington link to work page

--- client/src/Work.js
+++ client/src/Work.js
@@ -10,6 +10,7 @@ export default function Work() {
                 </div>
                 <div>
                     <ul className="portfolio-list-grid">
+                        <li className="portfolio-list-item"><a className="portfolio-list-link" href="https://park-burlington.herokuapp.com/"><img className="portfolio-list-image" src="/images/parkburlington_thumbnail.jpg" alt="parkburlington page"></img>Park Burlington</a></li>
                         <li className="portfolio-list-item"><a className="portfolio-list-link" href="https://hidden-fortress-73610.herokuapp.com/"><img className="portfolio-list-image" src="/images/mockblog_thumbnail.jpg" alt="yelpington page"></img>Mock Blog</a></li>
                         <li className="portfolio-list-item"><a className="portfolio-list-link" href="https://secure-fjord-65417.herokuapp.com/"><img className="portfolio-list-image" src="/images/yelpington_thumbnail.jpg" alt="yelpington page"></img>Yelpington</a></li>
                         <li className="portfolio-list-item"><a className="portfolio-list-link" href="https://stark-plateau-02827.herokuapp.com/"><img className="portfolio-list-image" src="/images/triptactoe_thumbnail.jpg" alt="trip-tac-toe game"></img>Trip-Tac-Toe</a></li>
