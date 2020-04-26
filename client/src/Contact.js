e7b86c2e8741ff2aa87cdc85af8a4e55047d629a
JonChamplain <47443316+JonChamplain@users.noreply.github.com>
2020-04-25 22:30:07 -0400

    improved mobile responsiveness

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -11,9 +11,9 @@ export default function Contact() {
 
                 <div>
                     <ul className="contact-list-grid">
-                        <li className="contact-list-item"><a className="contact-list-link" href="mailto:joninvermont@gmail.com"><img className="contact-list-image" src="https://image.flaticon.com/icons/svg/732/732200.svg" alt="gmail"></img>joninvermont@gmail.com</a></li>
-                        <li className="contact-list-item"><a className="contact-list-link" href="https://www.linkedin.com/in/jonathan-russell-76a0ab71/"><img className="contact-list-image" src="https://image.flaticon.com/icons/svg/2111/2111499.svg" alt="linked in"></img>LinkedIn</a></li>
-                        <li className="contact-list-item"><a className="contact-list-link" href="https://github.com/JonChamplain"><img className="contact-list-image" src="https://image.flaticon.com/icons/svg/733/733553.svg" alt="GitHub"></img>GitHub</a></li>
+                        <li className="contact-list-item"><a className="contact-list-link" href="mailto:joninvermont@gmail.com"><img className="contact-list-image" src="https://image.flaticon.com/icons/svg/732/732200.svg" alt="gmail"></img><span class="contact-media-label">joninvermont@gmail.com</span></a></li>
+                        <li className="contact-list-item"><a className="contact-list-link" href="https://www.linkedin.com/in/jonathan-russell-76a0ab71/"><img className="contact-list-image" src="https://image.flaticon.com/icons/svg/2111/2111499.svg" alt="linked in"></img><span class="contact-media-label">LinkedIn</span></a></li>
+                        <li className="contact-list-item"><a className="contact-list-link" href="https://github.com/JonChamplain"><img className="contact-list-image" src="https://image.flaticon.com/icons/svg/733/733553.svg" alt="GitHub"></img><span class="contact-media-label">GitHub</span></a></li>
                     </ul>
                 </div>
             </div>
